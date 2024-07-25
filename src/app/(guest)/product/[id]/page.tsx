83f4de2b1b5a940cc309ed6501237e91405adf86
Mali <kul.mehmetali@yahoo.com>
2024-07-25 14:13:52 +0300

    implementing useparams

--- src/app/(guest)/product/[id]/page.tsx
+++ src/app/(guest)/product/[id]/page.tsx
@@ -3,13 +3,22 @@ import { getProduct } from '@/actions/get-products'
 import { Button } from '@/components/ui/button'
 import { Skeleton } from '@/components/ui/skeleton'
 import { ShoppingCart } from 'lucide-react';
-import { FaHeart ,FaRegHeart } from "react-icons/fa";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { useSearchParams } from 'next/navigation';
 
 type Props = {}
 
 const Product = async ({ params }: { params: { id: string } }) => {
-    /*  const params = useParams() */
+
     const product = await getProduct(params.id)
+    const searchParams = useSearchParams()
+    const createQueryString = (key: string, value: number | string) => {
+        const params = new URLSearchParams(searchParams)
+        params.set(key, String(value))
+    
+        return String(params)
+      }
+
     return (
         <div className="grid grid-rows-12 grid-cols-12 gap-x-20 h-full">
             <div className="row-span-12 col-span-6 bg-white h-full">
@@ -40,8 +49,11 @@ const Product = async ({ params }: { params: { id: string } }) => {
                 <div>{product ? product.description : <Skeleton className="h-24 w-[700px]" />}</div>
             </div>
             <div className='flex items-center justify-end row-span-2 col-span-6 size-full'>
-                <FaRegHeart className='mr-8 size-8 text-red-500'/>
-                <Button className='px-6 py-6 flex items-center'><ShoppingCart className='mr-4'/> ADD TO CART</Button>
+                <div className='p-2 rounded-full hover:cursor-pointer hover:bg-pink-50 mr-4 flex items-center justify-center'>
+                    <FaRegHeart className='size-8 text-red-500' />
+                </div>
+
+                <Button className='px-6 py-6 flex items-center'><ShoppingCart className='mr-4' /> ADD TO CART</Button>
             </div>
         </div>
     )
